3822b5b6e6fbec56952af9b17c25ccfb740af7f5
huotianjun <huotianjun@gmail.com>
2016-10-11 18:13:39 +0800

    commit

--- examples/Java/WebmqxServer.java
+++ examples/Java/WebmqxServer.java
@@ -3,66 +3,96 @@ import org.json.*;
 import java.io.IOException;
 
 public class WebmqxServer {
+	private static String handle(String message) {
+		JSONObject rpc_request = new JSONObject(message);
+		JSONObject http_req = rpc_request.getJSONObject("req");
+		String http_host = http_req.getString("host");
+		String http_method = http_req.getString("method");
+		String http_path = http_req.getString("path");
+		String http_qs = http_req.getString("qs");
+
+		System.out.println(" req " +  http_req.toString());
+
+		String http_body = rpc_request.getString("body");
+		System.out.println(" body " + http_body );
+				
+		//
+		// Write your codes here
+		//
+		return "HelloWorld";
+	}
+
 	public static void main(String[] argv) throws Exception {
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setUsername("guest");
-		factory.setPassword("guest");
-		factory.setHost("localhost");
+		Connection connection = null;
+		Channel channel = null;
+		try {
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setUsername("guest");
+			factory.setPassword("guest");
+			factory.setHost("localhost");
 
-		Connection connection = factory.newConnection();
-		final Channel channel = connection.createChannel();
+			connection = factory.newConnection();
+			Channel channel = connection.createChannel();
 
-		String queueName = channel.queueDeclare().getQueue();
-		channel.queueBind(queueName, "webmqx", "/Java/1");
-		channel.queueBind(queueName, "webmqx", "/Java/1/2");
-		channel.queueBind(queueName, "webmqx", "/Java/1/2/3");
-		channel.queueBind(queueName, "webmqx", "/Java/3/2/1");
+			String queueName = channel.queueDeclare().getQueue();
+			channel.queueBind(queueName, "webmqx", "/Java/1");
+			channel.queueBind(queueName, "webmqx", "/Java/1/2");
+			channel.queueBind(queueName, "webmqx", "/Java/1/2/3");
+			channel.queueBind(queueName, "webmqx", "/Java/3/2/1");
 
-		channel.basicQos(1);
+			channel.basicQos(1);
 
-		System.out.println(" [x] Awaiting HTTP requests");
+			QueueingConsumer consumer = new QueueingConsumer(channel);
+			channel.basicConsume(queueName, false, consumer);
 
-		Consumer consumer = new DefaultConsumer(channel) {
-			@Override
-			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-			throws IOException {
-				String message = new String(body, "UTF-8");
-				System.out.println(" [x] Received '" + message + "'");
+			System.out.println(" [x] Awaiting HTTP requests");
 
-				JSONObject rpc_request = new JSONObject(message);
-				JSONObject http_req = rpc_request.getJSONObject("req");
-				String http_host = http_req.getString("host");
-				String http_method = http_req.getString("method");
-				String http_path = http_req.getString("path");
-				String http_qs = http_req.getString("qs");
+			while (true) {
+				String response = null;
+				String response_string = null;
+				QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+				        
+				BasicProperties props = delivery.getProperties();
+				BasicProperties replyProps = new BasicProperties
+																.Builder()
+																.correlationId(props.getCorrelationId())
+																.build();
 
-				System.out.println(" req " +  http_req.toString());
+				try {
+					String message = new String(delivery.getBody(),"UTF-8");
+					System.out.println(" [x] Received '" + message + "'");
 
-				String http_body = rpc_request.getString("body");
-				System.out.println(" body " + http_body );
-				
-				//
-				// Write your codes here
-				//
-				String response = "HelloWorld" ;
+					String response = handle(message); 
 
-				JSONObject response_body = new JSONObject();
-				JSONObject headers_body = new JSONObject();
-				headers_body.put("content-type", "text/html");
-				response_body.put("headers", headers_body);
-				response_body.put("body", response);
+					JSONObject response_body = new JSONObject();
+					JSONObject headers_body = new JSONObject();
+					headers_body.put("content-type", "text/html");
+					response_body.put("headers", headers_body);
+					response_body.put("body", response);
 
-				String response_string = response_body.toString();
-				System.out.println(" response " + response_string );
-				
-				AMQP.BasicProperties replyProps = new AMQP.BasicProperties.Builder().correlationId(properties.getCorrelationId()).build();
-				channel.basicPublish( "", properties.getReplyTo(), replyProps, response_string.getBytes());
-				channel.basicAck(envelope.getDeliveryTag(), false);
-			};
-		};
-
-		while (true) {
-			channel.basicConsume(queueName, true, consumer);
+					response_string = response_body.toString();
+					System.out.println(" response " + response_string );
+				}
+				catch (Exception e){
+					System.out.println(" [.] " + e.toString());
+					response = "";
+				}
+				finally {  
+					channel.basicPublish( "", props.getReplyTo(), replyProps, response_string.getBytes("UTF-8"));
+					channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+				}
+			}
 		}
+		catch  (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (Exception ignore) {}
+				}
+		}  
 	}
 }
