965cb2c38e44e69a5186f9dfb53383bf1dc71a04
huotianjun <huotianjun@gmail.com>
2016-10-11 12:44:53 +0800

    commit

---
+++ examples/Java/WebmqxServer.java
@@ -0,0 +1,46 @@
+import com.rabbitmq.client.*;
+import java.io.IOException;
+
+public class WebmqxServer {
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUsername("guest");
+		factory.setPassword("guest");
+		factory.setHost("localhost");
+
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		String queueName = channel.queueDeclare().getQueue();
+		channel.queueBind(queueName, "webmqx", "/Java/1");
+		channel.queueBind(queueName, "webmqx", "/Java/1/2");
+		channel.queueBind(queueName, "webmqx", "/Java/1/2/3");
+		channel.queueBind(queueName, "webmqx", "/Java/3/2/1");
+
+		channel.basicQos(1);
+
+		System.out.println(" [x] Awaiting HTTP requests");
+
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+			throws IOException {
+				String message = new String(body, "UTF-8");
+				System.out.println(" [x] Received '" + message + "'");
+				BasicProperties replyProps = new BasicProperties
+												.Builder()
+												.correlationId(properties.getCorrelationId())
+												.build();
+				
+				//
+				// Write your codes here
+				//
+				String response = "HelloWorld" ;
+	
+				channel.basicPublish( "", properties.getReplyTo(), replyProps, response.getBytes());
+				channel.basicAck(enverlope.getDeliveryTag(), false);
+			};
+
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+	}
+}
