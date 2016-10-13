ce76081fc528a611235f7dafc482e49ab0b187ad
huotianjun <huotianjun@gmail.com>
2016-10-13 16:35:50 +0800

    commit

--- examples/php/pthreads/webmqx-server.php
+++ examples/php/pthreads/webmqx-server.php
@@ -1,118 +1,70 @@
 <?php
-class Handler extends Thread {
-	public function __construct(AMQPEnvelope $message)
-	{
-		$this->message = $message;
-	}
-
-	public function run(){
-		// Establish connection to AMQP for rpc response.
-		// attension: don't use $this->amqp_connection.
-		$amqp_connection = new AMQPConnection();
-		$amqp_connection->setHost('127.0.0.1'); // Webmqx Server IP.
-		$amqp_connection->setLogin('guest');
-		$amqp_connection->setPassword('guest');
-		$amqp_connection->connect();
-		$amqp_channel = new AMQPChannel($amqp_connection);
-		$amqp_exchange = new AMQPExchange($amqp_channel);
-		
-		// Parse webmqx rpc request message.
-		$rpc_request = json_decode($this->message->getBody(), true);
-		$http_body = $rpc_request['body'];
-		$http_request = $rpc_request['req'];
-		$http_path = $http_request['path'];
-		$http_query = $http_request['qs'];
-
-		$attributes = array(
-							'correlation_id' => $this->message->getCorrelationId()
-						);
-
-		$result = $this->handle($http_path, $http_query, $http_body);
-
-		// Return for http response.
-		$amqp_exchange->publish((string)($result),
-									$this->message->getReplyTo(), 
-									AMQP_NOPARAM,
-									$attributes
-								);
-
-		$amqp_connection->disconnect();
-	}
-
-	public function handle($http_path, $http_query, $http_body) {
-		//
-		// Your codes would be written in here.
-		//
-
-		$response_body = 'Hello World';	
-
-		$response = array (
-							'headers' => array ( 'content-type' => 'text/html') ,
-							'body' => $response_body 
-						);
-		
-		return json_encode($response);
-	}
-}
-
-class WebmqxServer {
-	private $connection;
-	private $channel;
-	private $queue;
-	private $exchange;
+class WebmqxServer extends Thread{
 
 	public function __construct()
 	{
 	}
-	
-	private function amqp_connect() {
-		//Establish connection to AMQP to consume rpc request.
-		$this->connection = new AMQPConnection();
-		$this->connection->setHost('127.0.0.1');
-		$this->connection->setLogin('guest');
-		$this->connection->setPassword('guest');
-		$this->connection->connect();
+
+	public function run(){
+		//Establish connection to AMQP
+		$connection = new AMQPConnection();
+		$connection->setHost('127.0.0.1');
+		$connection->setLogin('guest');
+		$connection->setPassword('guest');
+		$connection->connect();
 
 		//Declare Channel
-		$this->channel = new AMQPChannel($this->connection);
-		$this->channel->setPrefetchCount(1);
+		$channel = new AMQPChannel($connection);
+		$channel->setPrefetchCount(1);
 
-		// This is a non-consistent queue, just for this process.
-		$this->queue = new AMQPQueue($this->channel);
-		$this->queue->setFlags(AMQP_EXCLUSIVE | AMQP_AUTODELETE);
-		$this->queue->declareQueue();
+		$queue = new AMQPQueue($channel);
+		$queue->setFlags(AMQP_EXCLUSIVE | AMQP_AUTODELETE);
+		$queue->declareQueue();
 
-		// Exchange must be set to  'webmqx'.
+		// Exchange must be set to  'webmqx'. 
 		$exchange_name = 'webmqx';
 
-		// There can set many paths what you want to handle.
+		// There can set many paths which you want to handle.
 		$binding_key1 = '/php-test/1';
 		$binding_key2 = '/php-test/1/2';
 		$binding_key3 = '/php-test/1/2/3';
 		$binding_key4 = '/php-test/3/2/1';
 
-		$this->queue->bind($exchange_name, $binding_key1);
-		$this->queue->bind($exchange_name, $binding_key2);
-		$this->queue->bind($exchange_name, $binding_key3);
-		$this->queue->bind($exchange_name, $binding_key4);
-	}
+		$queue->bind($exchange_name, $binding_key1);
+		$queue->bind($exchange_name, $binding_key2);
+		$queue->bind($exchange_name, $binding_key3);
+		$queue->bind($exchange_name, $binding_key4);
 
-	public function init() {
+		$exchange = new AMQPExchange($channel);
 
-		$this->amqp_connect();	
+		$server = $this;
 
-		// This callback to handle a webmqx http_rpc_request.
-		$callback_func = function(AMQPEnvelope $message, AMQPQueue $q) {
-			// A thread, a rpc request.
-			$handler = new Handler($message);
-			$handler->start() && $handler->join();
+		$callback_func = function(AMQPEnvelope $message, AMQPQueue $q) use ($exchange, $server) {
+			$rpc_request = json_decode($message->getBody(), true);
+			$http_request = $rpc_request['req'];
+			$http_body = $rpc_request['body'];
+			$http_path = $http_request['path'];
+			$http_query = $http_request['qs'];
+
+			$attributes = array(
+								'correlation_id' => $message->getCorrelationId()
+								);
+
+			$result = $server->handle($http_path, $http_query, $http_body);
+
+			$exchange->publish(	(string)($result),
+									$message->getReplyTo(), 
+									AMQP_NOPARAM,
+									$attributes
+								);
+			
 			$q->ack($message->getDeliveryTag());
 		};
 		
 		$continue = True;
 		while($continue){
 			try {
-				$this->queue->consume($callback_func);
+				$queue->consume($callback_func);
 			} catch(AMQPQueueException $ex) {
 				print_r($ex);
 				$continue = False;
@@ -121,11 +73,33 @@ class WebmqxServer {
 				$continue = False;
 			}
 		}
-		$this->connection->disconnect();
+		$connection->disconnect();
 	}		
+
+	public function handle($http_path, $http_query, $http_body) {
+
+		//
+		// Your codes would be written in here.
+		//
+		$response_body = 'Hello World';
+
+		$response = array (
+						'headers' => array ( 'content-type' => 'text/html') ,
+						'body' => $response_body
+					   );
+		   
+		return json_encode($response);
+	}
 }
 
-$WebmqxServer = new WebmqxServer;
-$WebmqxServer->init() or print 'no request data';
+//
+// Here, startup 3 threads. you can start more.
+$handler = new WebmqxServer();
+$handler->start() && $handler->join();
 
+$handler2 = new WebmqxServer();
+$handler2->start() && $handler2->join();
+
+$handler3 = new WebmqxServer();
+$handler3->start() && $handler3->join();
 ?>
