db363d1b53d9ffb59294a4493ac8ba2682cb36bc
huotianjun <huotianjun@gmail.com>
2016-10-26 14:56:26 +0800

    routing by consistent hash ring.

--- src/webmqx_rpc_worker.erl
+++ src/webmqx_rpc_worker.erl
@@ -204,7 +204,6 @@ internal_rpc_publish(Path, Payload, From,
 			Publish = #'basic.publish'{exchange = <<"">>, 
                                routing_key = QueueName,
                                mandatory = true},
-
 			amqp_channel:call(Channel, Publish, #amqp_msg{props = Props,
                                                   payload = Payload})
 	end,
