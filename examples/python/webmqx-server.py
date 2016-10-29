b17da62b66a3ada1a57d59a567fde06139d23ea9
huotianjun <huotianjun@gmail.com>
2016-10-29 10:36:53 +0800

    connection

--- examples/python/webmqx-server.py
+++ examples/python/webmqx-server.py
@@ -32,7 +32,7 @@ def on_request(ch, method, props, body):
 
 def webmqx_server():
     # If not running on localhost of RabbitMQ server, don't use the user of 'guest', but try other. 
-    connection = pika.BlockingConnection(pika.URLParameters('amqp://guest:guest@127.0.0.1:5672/%2F?heartbeat_in    terval=1')) 
+    connection = pika.BlockingConnection(pika.URLParameters('amqp://guest:guest@127.0.0.1:5672/%2F?heartbeat_interval=1')) 
 
     channel = connection.channel()
     queue = channel.queue_declare(exclusive=True, auto_delete=True).method.queue
